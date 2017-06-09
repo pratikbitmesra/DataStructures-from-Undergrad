5415d74d1fce02a6440a5dd90f5e54d9bc1c5fce
Pratik Mishra <pratikbitmesra@users.noreply.github.com>
2017-06-09 08:37:32 -0500

    Create build_tree_inorder_preorder.py

--- build_tree_inorder_preorder.py
+++ build_tree_inorder_preorder.py
@@ -6,7 +6,9 @@ Let us consider the below traversals:
 Inorder sequence: D B E A F C
 Preorder sequence: A B D E C F
 
-In a Preorder sequence, leftmost element is the root of the tree. So we know ‘A’ is root for given sequences. By searching ‘A’ in Inorder sequence, we can find out all elements on left side of ‘A’ are in left subtree and elements on right are in right subtree. So we know below structure now.
+In a Preorder sequence, leftmost element is the root of the tree. So we know ‘A’ is root for given sequences. By searching ‘A’ in 
+Inorder sequence, we can find out all elements on left side of ‘A’ are in left subtree and elements on right are in right subtree.
+So we know below structure now.
 
                  A
                /   \
@@ -22,7 +24,8 @@ We recursively follow above steps and get the following tree.
  /     \    /
 D       E  F
 Algorithm: buildTree()
-1) Pick an element from Preorder. Increment a Preorder Index Variable (preIndex in below code) to pick next element in next recursive call.
+1) Pick an element from Preorder. Increment a Preorder Index Variable (preIndex in below code) to pick next element in next
+recursive call.
 2) Create a new tree node tNode with the data as picked element.
 3) Find the picked element’s index in Inorder. Let the index be inIndex.
 4) Call buildTree for elements before inIndex and make the built tree as left subtree of tNode.
