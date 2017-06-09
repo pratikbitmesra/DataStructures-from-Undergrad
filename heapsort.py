03528a53ac2e3f0379a2da017717d8f90210fd03
Pratik Mishra <pratikbitmesra@users.noreply.github.com>
2017-06-09 11:20:23 -0500

    Create heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -3,10 +3,12 @@ Heap Sort
 http://quiz.geeksforgeeks.org/heap-sort/
 Heap Sort Algorithm for sorting in increasing order:
 1. Build a max heap from the input data.
-2. At this point, the largest item is stored at the root of the heap. Replace it with the last item of the heap followed by reducing the size of heap by 1. Finally, heapify the root of tree.
+2. At this point, the largest item is stored at the root of the heap. Replace it with the last item of the heap followed 
+by reducing the size of heap by 1. Finally, heapify the root of tree.
 3. Repeat above steps while size of heap is greater than 1.
 How to build the heap?
-Heapify procedure can be applied to a node only if its children nodes are heapified. So the heapification must be performed in the bottom up order.
+Heapify procedure can be applied to a node only if its children nodes are heapified. So the heapification 
+must be performed in the bottom up order.
 Lets understand with the help of an example:
 Input data: 4, 10, 3, 5, 1
                  4(0)
